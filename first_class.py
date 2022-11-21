0e86d245eaad7e9c37f17b98f3d841bb3e7d497b
sirdesmond09 <sirdesmond09@gmail.com>
2022-11-21 11:19:51 +0100

    first class

---
+++ first_class.py
@@ -0,0 +1,18 @@
+# my_number = 10
+
+# to_string = str(my_number)
+
+
+# print(type(to_string))
+
+# x = 15
+# print(4 * (x**2) + 2*x + 1)
+
+a= 4
+b = 2
+c = 1
+
+numerator = -b + ( (b**2) - (4*a*c) )**0.5
+denominator = 2*a
+
+print(numerator/denominator)
